0187710b43458cafd8c6b8f6043b964f3923d922
feng <1423088852@qq.com>
2026-03-26 15:45:02 +0800

    添加内容

---
+++ 06.numpy/Ndarray函数.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+arr1 = np.arange(10)
+print(arr1)
+
+arr2 = np.arange(5,15,2)
+print(arr2)
+
+arr3 = np.arange(0,1,0.1)
+print(arr3)
+
+arr4 = np.arange(10,dtype=np.float32)
+print(arr4)
+
+
+arr5 = np.zeros(5)
+print(arr5)
+
+arr6 = np.zeros((3,2))
+print(arr6)
+
+arr7=np.zeros((3,2,4))
+print(arr7)
---
+++ 06.numpy/创建Ndarray.py
@@ -0,0 +1,15 @@
+import numpy as np
+arr1 = np.array([1,2,3,4,5])
+print(arr1)
+
+lis1 = [1,2,3,4,5]
+arr2 = np.array(lis1)
+print(type(arr2))
+print(arr2)
+
+arr3 =np.array([[1,2,3],[4,5,6]])
+print(type(arr3))
+print(arr3)
+
+arr4 = np.array([[[1,2,3],[4,5,6],],[[1,2,3],[4,5,6]]])
+print(arr4)
