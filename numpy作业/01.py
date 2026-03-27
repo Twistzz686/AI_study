9ea9cd4a5bb85feb944b41791cac7c893150642f
feng <1423088852@qq.com>
2026-03-27 11:56:20 +0800

    添加新内容

---
+++ numpy作业/01.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr1 = np.arange(0,20,2)
+print(arr1)
+arr2 = np.ones((3,4),dtype = np.float32)
+print(arr2)
+arr3 = np.zeros((2,3,4))
+print(arr3)
+arr4 = np.full((2,3),9)
+print(arr4)
